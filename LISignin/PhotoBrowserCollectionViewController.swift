a84ac8f6ac3a8828943f965108a3b5e65514254d
Cassiszuoan <jerry831210@gmail.com>
2016-05-09 15:21:47 +0800

    Add SearchBar

--- LISignin/PhotoBrowserCollectionViewController.swift
+++ LISignin/PhotoBrowserCollectionViewController.swift
@@ -27,7 +27,7 @@ struct Media {
 
 
 
-class PhotoBrowserCollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, SKPhotoBrowserDelegate {
+class PhotoBrowserCollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate,UISearchBarDelegate,SKPhotoBrowserDelegate {
 
    
     
@@ -35,16 +35,29 @@ class PhotoBrowserCollectionViewController: UIViewController, UICollectionViewDa
     var medialist = [Media]()
     var images = [SKPhoto]()
     var urlList = [NSURL]()
+    var dataSourceForSearchResult:[Media]?
+    var searchBarBoundsY:CGFloat?
     var refreshControl =  UIRefreshControl()
+    var searchController: UISearchController!
+    var searchBarActive:Bool = false
     
+    @IBOutlet weak var SearchBarView: UIView!
+
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    @IBOutlet weak var SearchBar: UISearchBar!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.dataSource = self
         collectionView.delegate = self
+        SearchBar.delegate = self
         
         self.collectionView.alwaysBounceVertical = true
+        
+        
         
         getMediaFromInstagram()
         
@@ -60,6 +73,9 @@ class PhotoBrowserCollectionViewController: UIViewController, UICollectionViewDa
     }
     
     
+    
+    
+    
     override func viewDidAppear(animated: Bool) {
         
         self.refreshControl.addTarget(self, action:#selector(PhotoBrowserCollectionViewController.refreshcell(_:)), forControlEvents: .ValueChanged)
@@ -67,6 +83,91 @@ class PhotoBrowserCollectionViewController: UIViewController, UICollectionViewDa
         self.collectionView.addSubview(refreshControl)
     }
     
+    // MARK: Search
+    func filterContentForSearchText(searchText:String){
+        
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        // user did type something, check our datasource for text that looks the same
+        if searchText.characters.count > 0 {
+            // search and reload data source
+            self.searchBarActive    = true
+            self.filterContentForSearchText(searchText)
+            self.collectionView?.reloadData()
+        }else{
+            // if text lenght == 0
+            // we will consider the searchbar is not active
+            self.searchBarActive = false
+            self.collectionView?.reloadData()
+        }
+        
+    }
+    
+    // MARK: Configure SearchBar
+
+    
+    
+    
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        self .cancelSearching()
+        self.collectionView?.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        self.searchBarActive = true
+        self.view.endEditing(true)
+    }
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        // we used here to set self.searchBarActive = YES
+        // but we'll not do that any more... it made problems
+        // it's better to set self.searchBarActive = YES when user typed something
+        self.SearchBar!.setShowsCancelButton(true, animated: true)
+    }
+    
+    func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+        // this method is being called when search btn in the keyboard tapped
+        // we set searchBarActive = NO
+        // but no need to reloadCollectionView
+        self.searchBarActive = false
+        self.SearchBar!.setShowsCancelButton(false, animated: false)
+    }
+    func cancelSearching(){
+        self.searchBarActive = false
+        self.SearchBar!.resignFirstResponder()
+        self.SearchBar!.text = ""
+    }
+    
+    
+    func addObservers(){
+        let context = UnsafeMutablePointer<UInt8>(bitPattern: 1)
+        self.collectionView?.addObserver(self, forKeyPath: "contentOffset", options: [.New,.Old], context: context)
+    }
+    
+    func removeObservers(){
+        self.collectionView?.removeObserver(self, forKeyPath: "contentOffset")
+    }
+    
+    
+    override func observeValueForKeyPath(keyPath: String?,
+                                         ofObject object: AnyObject?,
+                                                  change: [String : AnyObject]?,
+                                                  context: UnsafeMutablePointer<Void>){
+        if keyPath! == "contentOffset" {
+            if let collectionV:UICollectionView = object as? UICollectionView {
+                self.SearchBar?.frame = CGRectMake(
+                    self.SearchBar!.frame.origin.x,
+                    self.searchBarBoundsY! + ( (-1 * collectionV.contentOffset.y) - self.searchBarBoundsY!),
+                    self.SearchBar!.frame.size.width,
+                    self.SearchBar!.frame.size.height
+                )
+            }
+        }
+    }
+        //MARK: Segue
+        
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "unwindToLogin" {
@@ -81,15 +182,20 @@ class PhotoBrowserCollectionViewController: UIViewController, UICollectionViewDa
     func refreshcell(refreshControl: UIRefreshControl){
        
         
-        medialist.removeAll()
-        images.removeAll()
-        
-        getMediaFromInstagram()
         
         
+        let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(1 * Double(NSEC_PER_SEC)))
+        dispatch_after(delayTime, dispatch_get_main_queue()) {
+            // stop refreshing after 2 seconds
+            self.medialist.removeAll()
+            self.images.removeAll()
+            self.getMediaFromInstagram()
+            refreshControl.endRefreshing()
+        }
         
-        refreshControl.endRefreshing()
     }
+    
+    // MARK: 登出
     
     @IBAction func Logout(sender: AnyObject) {
         
@@ -117,7 +223,7 @@ class PhotoBrowserCollectionViewController: UIViewController, UICollectionViewDa
     
     
        
-    
+    // MARK: Get Data From Instagram API
     func getMediaFromInstagram(){
         
         
